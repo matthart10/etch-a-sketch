553c8daed3c83f4fd378967c929e67218b6e4350
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-29 18:50:18 -0700

    Force 16x16 grid by adding min-width

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let container = document.querySelector("#container");
-container.style.cssText = "display: flex; max-width: 900px; flex-wrap: wrap"
+container.style.cssText = "display: flex; min-width: 900px; max-width: 900px; flex-wrap: wrap"
 for (let i=0; i < 256; i++) {
 
 const square = document.createElement("div");
+square.classList.add("box");
 square.style.cssText = "border-style: solid; border-width: 3px; border-color: black; width: 50px; height: 50px;";
 container.appendChild(square);
 
 }
+
+let boxes = document.querySelectorAll(".box");
