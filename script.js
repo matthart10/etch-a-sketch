3844891fb9c54fdfb0287d3d9d8f57018b4ea34f
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-30 18:58:10 -0700

    Create container for where grid will be

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let container = document.querySelector("#container");
-container.style.cssText = "display: flex; min-width: 960px; max-width: 960px; flex-wrap: wrap"
 
 function startPrompt() {
     var gridSize = prompt("What size grid do you want?", "0")
@@ -9,7 +8,7 @@ function startPrompt() {
     }
     return gridSize;
 }
-
+// makeGrid(gridSize);
 
 let startButton = document.createElement("button");
 startButton.id = "btn"
@@ -17,16 +16,22 @@ startButton.textContent = "Click to choose size grid";
 container.appendChild(startButton);
 startButton.addEventListener("click", startPrompt);
 
+let smallContainer = document.createElement("div");
+smallContainer.style.cssText = "min-width: 960px; max-width: 960px; display: flex; flex-wrap: wrap;";
+container.appendChild(smallContainer);
 
 
-for (let i=0; i < 256; i++) {
-
-const square = document.createElement("div");
-square.classList.add("box");
-square.style.cssText = "border-style: solid; border-width: 3px; border-color: black; width: 50px; height: 50px;";
-container.appendChild(square);
-
+function makeGrid(number) {
+    for (let i=0; i < 256; i++) {
+    
+        const square = document.createElement("div");
+        square.classList.add("box");
+        square.style.cssText = "border-style: solid; border-width: 3px; border-color: black; width: 50px; height: 50px;";
+        smallContainer.appendChild(square);
+        
+        }
 }
+
 
 let boxes = document.querySelectorAll(".box");
 boxes.forEach((div) => {
