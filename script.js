8c82843083eec165830e8ba3b97c850d061c29a5
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-30 18:47:22 -0700

    Create button and function with prompt
    Create a button that will launch a function when clicked that prompts
    the user to enter a number. If not entering a number, then user
    enters a while loop until they do. This function returns the user's
    number. This will be used to make the grid size.

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let container = document.querySelector("#container");
-container.style.cssText = "display: flex; min-width: 900px; max-width: 900px; flex-wrap: wrap"
+container.style.cssText = "display: flex; min-width: 960px; max-width: 960px; flex-wrap: wrap"
+
+function startPrompt() {
+    var gridSize = prompt("What size grid do you want?", "0")
+    while (isNaN(gridSize)) {
+        alert("Not a Number. Try again.");
+        gridSize = prompt("What size grid do you want?", "0")
+    }
+    return gridSize;
+}
+
+
+let startButton = document.createElement("button");
+startButton.id = "btn"
+startButton.textContent = "Click to choose size grid";
+container.appendChild(startButton);
+startButton.addEventListener("click", startPrompt);
+
+
+
 for (let i=0; i < 256; i++) {
 
 const square = document.createElement("div");
