3de1a1e3d3510b60f5cc49fed778a16f0e0c21e5
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-08-01 07:23:48 -0700

    Add empty text content to clear grid
    Add empty text to smallContainer so it clears any elements inside
    this if there is anything

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 let container = document.querySelector("#container");
-
-var gridSize = 0;
-function startPrompt() {
-    gridSize = prompt("What size grid do you want?");
-    while (isNaN(gridSize)) {
-        alert("Not a Number. Try again.");
-        gridSize = prompt("What size grid do you want?");
-    }
-    let adjustedSize = 960 / gridSize;
-    makeGrid(gridSize, adjustedSize);
-}
-
-
 let startButton = document.createElement("button");
 startButton.id = "btn"
 startButton.textContent = "Click to choose size grid";
 container.appendChild(startButton);
 startButton.addEventListener("click", startPrompt);
+var gridSize = 0;
+
 
 let smallContainer = document.createElement("div");
 smallContainer.style.cssText = "min-width: 960px; max-width: 960px; display: flex; flex-wrap: wrap;";
 container.appendChild(smallContainer);
 
+function startPrompt() {
+    gridSize = prompt("What size grid do you want?");
+    while ((isNaN(gridSize)) || (gridSize > 100)) {
+        alert("Not a number or number is too large. Try again.");
+        gridSize = prompt("What size grid do you want?");
+    }
+    let adjustedSize = 960 / gridSize;
+
+    makeGrid(gridSize, adjustedSize);
+}
+
+
+
 function makeGrid(number1, number2) {
+    smallContainer.textContent = '';
     for (let i=0; i < (number1 * number1); i++) {
-    
         const square = document.createElement("div");
         square.classList.add("box");
         square.style.boxSizing = "border-box";
         square.style.width = number2 + "px";
         square.style.height = number2 + "px";
         smallContainer.appendChild(square);
-        
         }
         let boxes = document.querySelectorAll(".box");
         boxes.forEach((div) => {
